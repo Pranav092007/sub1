8903be170059b4ce7795b73a0207b60e0ed06f9c
Pranav Navghare <goodtechnologyg2858@gmail.com>
2025-09-03 19:41:31 +0530

    Convert website/index.html to React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="website-content">
+        {/* Content will be populated based on the actual website/index.html */}
+        <h1>Loading website content...</h1>
+        <p>Converting from website/index.html</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
